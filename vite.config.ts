10406110f002f0603b19c82cde605278f2d1f24d
Mostafa.Megria <megria.mostafa@gmail.com>
2025-09-18 09:24:35 +0300

    Add that command to change the application root directory to allow open in github pages.

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: "/bdc-bank-buddy/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),

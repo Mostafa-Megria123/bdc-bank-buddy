539230473cd5e5a6a82ad0c3db4d3a510e9386ae
Mostafa.Megria <megria.mostafa@gmail.com>
2025-09-18 15:42:38 +0300

    Refactor vite config.

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
-  base: "/bdc-bank-buddy/",
+  // base: "/bdc-bank-buddy/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
